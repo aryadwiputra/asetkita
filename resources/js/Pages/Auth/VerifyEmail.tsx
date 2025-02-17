5e04e8f7a54cd5ce6c2bfca9c20fd9b3a8a1b9c2
Arya Dwi Putra <aryadptr.developer@gmail.com>
2025-02-17 22:10:03 +0700

    Modify verify email page

--- resources/js/Pages/Auth/VerifyEmail.tsx
+++ resources/js/Pages/Auth/VerifyEmail.tsx
@@ -1,51 +1,79 @@
-import PrimaryButton from '@/Components/PrimaryButton';
-import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
-import { FormEventHandler } from 'react';
+import ApplicationLogo from "@/Components/ApplicationLogo/Index";
+import { Alert, AlertDescription } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import UnauthenticatedLayout from "@/Layouts/UnauthenticatedLayout";
+import { Link, useForm } from "@inertiajs/react";
 
 export default function VerifyEmail({ status }: { status?: string }) {
     const { post, processing } = useForm({});
 
-    const submit: FormEventHandler = (e) => {
+    const onHandleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
 
-        post(route('verification.send'));
+        post(route("verification.send"));
     };
 
     return (
-        <GuestLayout>
-            <Head title="Email Verification" />
-
-            <div className="mb-4 text-sm text-gray-600">
-                Thanks for signing up! Before getting started, could you verify
-                your email address by clicking on the link we just emailed to
-                you? If you didn't receive the email, we will gladly send you
-                another.
+        <>
+            <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
+                <div className="flex flex-col px-6 py-4">
+                    <ApplicationLogo size="size-12" />
+                    <div className="flex flex-col items-center justify-center py-12 lg:py-40">
+                        <div className="mx-auto flex w-full flex-col gap-6 lg:w-1/2">
+                            <div className="grid gap-2 text-center">
+                                {status === "verification-link-sent" && (
+                                    <Alert variant="success">
+                                        <AlertDescription>
+                                            Link verifikasi telah dikirim ke
+                                            email anda
+                                        </AlertDescription>
+                                    </Alert>
+                                )}
+                                <h1 className="text-3xl font-bold">
+                                    Verifikasi Email
+                                </h1>
+                                <p className="text-balance text-muted-foreground">
+                                    Sebelum melanjutkan, pastikan email anda
+                                    terverifikasi
+                                </p>
+                            </div>
+                            {/* Form */}
+                            <form onSubmit={onHandleSubmit}>
+                                <div className="grid gap-4">
+                                    <Button
+                                        size="xl"
+                                        className="w-full"
+                                        disabled={processing}
+                                    >
+                                        Kirim Ulang Link Verifikasi
+                                    </Button>
+                                </div>
+                            </form>
+                            <div className="mt-4 text-center text-sm">
+                                <Link
+                                    href={route("logout")}
+                                    method="post"
+                                    as="button"
+                                    className="underline"
+                                >
+                                    Logout
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-mued hidden lg:block">
+                    <img
+                        src="/images/login.jpg"
+                        alt="login"
+                        className="h-full w-full object-cover dark:brightness-[0.4] dark:grayscale-0"
+                    />
+                </div>
             </div>
-
-            {status === 'verification-link-sent' && (
-                <div className="mb-4 text-sm font-medium text-green-600">
-                    A new verification link has been sent to the email address
-                    you provided during registration.
-                </div>
-            )}
-
-            <form onSubmit={submit}>
-                <div className="mt-4 flex items-center justify-between">
-                    <PrimaryButton disabled={processing}>
-                        Resend Verification Email
-                    </PrimaryButton>
-
-                    <Link
-                        href={route('logout')}
-                        method="post"
-                        as="button"
-                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                        Log Out
-                    </Link>
-                </div>
-            </form>
-        </GuestLayout>
+        </>
     );
 }
+
+VerifyEmail.layout = (page: React.ReactNode) => (
+    <UnauthenticatedLayout children={page} title="Verifikasi Email" />
+);
